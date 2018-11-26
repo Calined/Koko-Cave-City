adb33fd723a8b493b3b09127a9990233288c301d
Chris Manthey <calined@gmx.de>
2018-11-26 17:37:43 +0100

    now camera moves to target position

---
+++ unity/Koko Cave City/Assets/CamTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTarget : MonoBehaviour
+{
+    IEnumerator FindNewPositionAndRotation(float secondsBetweenFinds)
+    {
+        transform.position = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        transform.Rotate(new Vector3(Random.Range(-180f, 180f), Random.Range(-180f, 180f), Random.Range(-180f, 180f)));
+
+        yield return new WaitForSeconds(secondsBetweenFinds);
+        StartCoroutine(FindNewPositionAndRotation(secondsBetweenFinds));
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(FindNewPositionAndRotation(100f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ unity/Koko Cave City/Assets/FollowCamTarget.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamTarget : MonoBehaviour
+{
+    public bool follow = true;
+    public Transform camTarget;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (follow)
+        {
+            if (Vector3.Distance(transform.position, camTarget.position) > 1f)
+            {
+                GetComponent<Rigidbody>().AddForce((camTarget.position - transform.position) * 2 * Time.deltaTime);
+            }
+
+            if (Vector3.Distance(transform.rotation.eulerAngles, camTarget.rotation.eulerAngles) > 100f)
+            {
+
+                //  GetComponent<Rigidbody>().AddTorque((camTarget.rotation.eulerAngles - transform.rotation.eulerAngles) * 10);
+            }
+        }
+    }
+}
